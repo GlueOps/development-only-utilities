4e982b4a907f6cbf116d28278b95f84c4c010ede
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-12 02:17:52 +0000

    fixes

--- tools/aws/account-setup.sh
+++ tools/aws/account-setup.sh
@@ -3,6 +3,7 @@
 ### Variables
 ##
 set -e
+echo -e "\n"
 [ "$(aws sts get-caller-identity --query Account --output text)" = "$(aws organizations describe-organization --query Organization.MasterAccountId --output text)" ] && echo -e "\e[32mTHIS IS THE ROOT ACCOUNT. PLEASE PROCEED\e[0m" || echo -e "\e[31mTHIS IS NOT THE ROOT ACCOUNT STOP IMMEDIATELY.\e[0m"
 echo -e "\n"
 echo "Please enter your AWS account name. It should start with glueops-captain (e.g. glueops-captain-laciudaddelgato):"
