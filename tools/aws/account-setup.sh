67ecddfb4f6beef2a7bdc9c7acf109d748f956a6
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-05-12 01:15:18 +0000

    fixes

--- tools/aws/account-setup.sh
+++ tools/aws/account-setup.sh
@@ -2,6 +2,7 @@
 ##
 ### Variables
 ##
+set -e
 SUB_ACCOUNT_ID=$(aws organizations list-accounts --output json | jq -r --arg ACCOUNT_NAME "$ACCOUNT_NAME" '.Accounts[] | select(.Name==$ACCOUNT_NAME) | .Id')
 IAM_USER_NAME="dev-deployment-svc-account"
 IAM_ROLE_NAME="captain-role"
