f65717634fea6e5a7d600044a873ae609b1043a6
Venkata Mutyala <venkata@venkatamutyala.com>
2023-12-12 13:40:58 -0800

    Update vault-nuke.sh

--- tools/aws/vault-nuke.sh
+++ tools/aws/vault-nuke.sh
@@ -3,7 +3,7 @@
 set -e
 echo -e "\n"
 
-[ "$(aws sts get-caller-identity --query Account --output text)" = "$(aws organizations describe-organization --query Organization.MasterAccountId --output text)" ] || echo -e "\e[31mTHIS IS NOT THE ROOT ACCOUNT STOP IMMEDIATELY.\e[0m" && exit 1
+[ "$(aws sts get-caller-identity --query Account --output text)" = "$(aws organizations describe-organization --query Organization.MasterAccountId --output text)" ] || (echo -e "\e[31mTHIS IS NOT THE ROOT ACCOUNT STOP IMMEDIATELY.\e[0m" && exit 1)
 echo -e "\n"
 echo -e "\e[31mTHIS SCRIPT WILL DELETE ALL YOUR BACKUPS FOR YOUR TENANT. This includes but isn't limited to loki backups and vault backups.\e[0m" 
 echo "Please enter your captain domain example: nonprod.earth.onglueops.rocks :"
