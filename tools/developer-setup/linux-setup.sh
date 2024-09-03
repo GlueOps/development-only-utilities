0470578c9c87ff863a6a45b1597a0914bbc00c57
Venkat <venkata@venkatamutyala.com>
2024-09-03 16:13:36 -0700

    Update linux-setup.sh (#36)

--- tools/developer-setup/linux-setup.sh
+++ tools/developer-setup/linux-setup.sh
@@ -35,7 +35,9 @@ sudo passwd -d $USER
 server_ip=$(echo $SSH_CONNECTION | awk '{print $3}')
 echo ""
 echo ""
-sudo figlet GlueOps | sudo tee /etc/motd
+#sudo figlet GlueOps | sudo tee /etc/motd
+{ echo -e "\e[1;32m$(figlet GlueOps)\e[0m"; echo ""; echo -e "\e[1;34mPlease log in as user 'vscode' or switch to that user by running:\e[0m"; echo ""; echo -e "\e[1;33m    sudo su - vscode\e[0m"; echo ""; echo -e "\e[1;34mAfter switching to the 'vscode' user, run the following command:\e[0m"; echo ""; echo -e "\e[1;33m    dev\e[0m"; } | sudo tee /etc/motd
+
 echo -e "\n\n\n\n\nThis machine is now being restarted and will disconnect your session. \n\n"
 
 sudo reboot
