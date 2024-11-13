0e599f5708162dbe960726e8691457027762c4a6
Venkat <venkata@venkatamutyala.com>
2024-11-14 04:44:04 +0530

    Update account-nuke.sh

--- tools/aws/account-nuke.sh
+++ tools/aws/account-nuke.sh
@@ -41,21 +41,6 @@ regions: #this regions list was last updated on October 10, 2023.
 - global
 - us-west-2
 - us-east-1
-- us-east-2
-- us-west-1
-- ap-south-1
-- ap-northeast-3
-- ap-northeast-2
-- ap-southeast-1
-- ap-southeast-2
-- ap-northeast-1
-- ca-central-1
-- eu-central-1
-- eu-west-1
-- eu-west-2
-- eu-west-3
-- eu-north-1
-- sa-east-1
 
 
 EOF
@@ -80,7 +65,7 @@ export AWS_SECRET_ACCESS_KEY=$secretAccessKey
 export AWS_DEFAULT_REGION=us-west-2
 
 
-./aws-nuke nuke -c nuke.yaml --max-wait-retries 200 --no-dry-run --force --log-level trace --log-full-timestamp true --log-caller true
+./aws-nuke nuke -c nuke.yaml --max-wait-retries 200 --no-dry-run --force --log-level debug --log-full-timestamp true --log-caller true
 
 rm nuke.yaml
 rm aws-nuke
